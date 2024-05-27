11c1f30a5cde5953af65e744995c5e155490a480
vostro <vostro@example.com>
2024-05-27 19:41:13 +0100

    Namespacing apps

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import HomePageView, AboutPageView
 
+app_name = 'pages'
 urlpatterns = [
     # About page
     path('about/', AboutPageView.as_view(), name='about'),
