996e49b97398dbbf99a4fdd7e833c9e9a33df8d8
vostro <vostro@example.com>
2024-05-28 20:12:08 +0100

    Working with Form in blog app.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView
+from .views import (BlogListView, 
+                    BlogDetailView, 
+                    BlogCreateView,
+                    BlogUpdateView,
+                    BlogDeletView
+)
 
 app_name = "blog"
 urlpatterns = [
+    # Create a blog post.
+    path("post/new/", BlogCreateView.as_view(), name="post_new"),
     # Display a single post.
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
-    # Home Page display all post.
+    # Update post.
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    # Delete post.
+    path("post/<int:pk>/delete/", BlogDeletView.as_view(), name='post_delete'),
+    # Home Page, display all post.
     path("", BlogListView.as_view(), name="home"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -12,3 +13,22 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "blog/post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "blog/post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeletView(DeleteView):
+    model = Post
+    template_name = "blog/post_delete.html"
+    success_url = reverse_lazy("blog:home")
+    
